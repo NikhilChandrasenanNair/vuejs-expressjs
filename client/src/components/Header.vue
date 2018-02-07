baf02ad75a063a4380b1ea93565f1f7752e96671
MCHQ\nchandrasenan <nchandrasenan@mcfadyen.com>
2018-02-07 20:00:03 +0530

    Added Logout

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <v-toolbar flat color="teal accent-4" fixed>
-    <router-link to="/">
+    <router-link to="/home">
       <v-toolbar-title class="white--text mr4">TabTracker</v-toolbar-title>
     </router-link>
     <!-- <v-toolbar-items>
@@ -9,17 +9,15 @@
       </v-btn>
     </v-toolbar-items> -->
     <v-spacer></v-spacer>
-    <v-toolbar-items v-if="!$store.state.isUserLoggedIn">
-      <router-link to="login">
+    <v-toolbar-items>
+      <router-link to="login" v-if="!$store.state.isUserLoggedIn">
         <v-btn flat dark class="mt-3">
           Sign In
         </v-btn>
       </router-link>
-        <!-- <router-link to="register">
-          <v-btn flat dark>
-            Sign Up
-          </v-btn>
-        </router-link> -->
+      <v-btn flat dark class="logout" v-if="$store.state.isUserLoggedIn" @click='logout'>
+        Logout
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -27,11 +25,20 @@
 <script>
 
 export default {
-
+  methods: {
+    logout () {
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: '/home'
+      })
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.logout {
+  padding-top: 5px;
+}
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '@/components/Register'
+import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: HelloWorld
+    },
     {
       path: '/register',
       name: 'register',
